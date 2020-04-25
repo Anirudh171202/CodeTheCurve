7401fbf4c1a4b8cb11f0eedede37cb7515223ec9
Anirudh171202 <anirudh.sathiya@gmail.com>
2020-04-25 18:19:42 +0530

    added server.py

--- server.py
+++ server.py
@@ -1,27 +1,44 @@
 from flask import abort, request, jsonify, Flask
-import bs4
+
 app = Flask(__name__) 
 import requests
 
 @app.route("/livecount", methods = ["POST", "GET"])
-def livecount():
+def receive():
     req = request.get_json()
     print(f"[REQ], {req}")
+    intentName= req["queryResult"]["intent"]
+    if intentName == "liveCount":
+        country = req["queryResult"]["parameters"]["country"]  
+        return livecount(country)
+    # elif intentName[0] == "Q":
 
-    country = req["queryResult"]["parameters"]["country"]
 
+        
+
+
+
+def livecount(country):
     try:
         
-        res = requests.get(f'https://api.covid19api.com/live/country/{country}/status/deaths').json()
-        print(f"[RES] {res[-1]}")
+        res = requests.get(f'https://api.covid19api.com/live/country/{country}/status/deaths').json()[-1]
+        print(f"[RES] {res}")
         
-        active = res[-1]["Active"]
+        active = res["Active"]
+        confirmed = res["Confirmed"]
+        recovered = res["Recovered"]
+        deaths = res["Deaths"]
+        date = res["Date"][:10]
         
-        message = f"They are {active} active cases in {country}" 
+        message = f'''As of {date},
+ there are {active} active cases, 
+ {confirmed} confirmed cases, 
+ {recovered} recovered patients, 
+ {deaths} deaths'''
         response = {
             "fulfillmentText": message,
             "fulfillmentMessages": [
-                {
+                {      
                     "text": {
                         "text": [ message ]
                     }
